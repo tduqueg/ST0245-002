a97b14338a48f21c39aec0ac3202d6452f80377c
felipeortizs <68928486+felipeortizs@users.noreply.github.com>
2020-08-24 13:18:13 -0500

    Add files via upload

---
+++ laboratorios/lab01/ejercicioEnLinea/EndX.java
@@ -0,0 +1,13 @@
+
+public class EndX
+{
+public String endX(String str) {
+
+  if (str.equals("")) return str;
+
+  if (str.charAt(0) == 'x') return endX(str.substring(1)) + 'x';
+
+  else return str.charAt(0) + endX(str.substring(1));
+
+}
+}
---
+++ laboratorios/lab01/ejercicioEnLinea/factorial.java
@@ -0,0 +1,9 @@
+
+public class factorial {
+public int factorial(int n) {
+  
+  if (n == 1) return 1;
+  
+  return n * factorial(n-1);
+}
+}
