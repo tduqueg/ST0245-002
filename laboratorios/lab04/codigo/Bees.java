9ab01278bfaadd0fed9c7d0d03b6ea802d2bf77f
tduqueg <68928364+tduqueg@users.noreply.github.com>
2020-11-03 06:34:28 -0500

    Add files via upload

---
+++ laboratorios/lab04/codigo/Bees.java
@@ -0,0 +1,21 @@
+public class Bees {
+
+        private double latitude;
+        private double longitude;
+        private double altitude;
+     public Bees(double latitude, double longitude, double altitude){
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.altitude=altitude;
+    }
+        public double getLatitude(){
+        return latitude;
+    }
+        public double getLongitude(){
+        return longitude;
+    }
+        public double getAltitude(){
+        return altitude;
+    }
+    }
+
